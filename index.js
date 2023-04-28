e57e8178737b5cba76562bca09b4b47d6de3865a
Prajwal Gade <96947201+Prajwal-45@users.noreply.github.com>
2023-04-28 20:57:42 +0530

    WheatherApp
    Dynamic Whether App

---
+++ index.js
@@ -0,0 +1,45 @@
+const http = require("http");
+const fs = require("fs");
+var requests = require("requests");
+const { json } = require("node:stream/consumers");
+
+const homeFile = fs.readFileSync("home.html", "utf-8");
+const replaceVal=(tempVal,orgVal)=>{
+    let temperatute=tempVal.replace("{%tempval%}",orgVal.main.temp);
+     temperatute=temperatute.replace("{%tempmin%}",orgVal.main.temp_min);
+     temperatute=temperatute.replace("{%tempmax%}",orgVal.main.temp_max);
+     temperatute=temperatute.replace("{%location%}",orgVal.name);
+     temperatute=temperatute.replace("{%country%}",orgVal.sys.country);
+     temperatute=temperatute.replace("{%tempstatus%}",orgVal.weather[0].main);
+   
+    return temperatute;
+    };
+
+const server = http.createServer((req, res) => {
+  if (req.url == "/") {
+    requests("https://api.openweathermap.org/data/2.5/weather?q=Pune&appid=411fda49daa41f7bd900338316b4fb6e")
+      .on('data',(chunk)=>{
+        const objData=JSON.parse(chunk);
+        const arrData=[objData];
+        // console.log(arrData[0].main.temp);
+        const realTimeData=arrData
+        .map((val)=> replaceVal(homeFile,val))
+        .join("");
+        res.write(realTimeData);
+          
+    })      
+    
+    .on('end',(err)=>{
+        if(err)return console.log("connection close due to error",err);
+       res.end();
+    });
+    
+  }
+  
+    
+  
+});
+
+server.listen(8000,'127.0.0.1',()=>{
+    console.log("hogayaaa");
+});
